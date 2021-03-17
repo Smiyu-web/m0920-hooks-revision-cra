66e20344496ae97b2a486d33d83778b71b947422
Miu <miu@Mius-MBP.hitronhub.home>
2021-03-17 15:05:08 -0700

    assignment done

--- src/components/WeatherApp/CityWeather.js
+++ src/components/WeatherApp/CityWeather.js
@@ -16,11 +16,15 @@ const CityWeather = (props) => {
   //   }
   // }
 
-  useEffect(async () => {
+  useEffect(() => {
+    getWeather();
+  }, [props.cityName]);
+
+  const getWeather = async () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
     const res = await axios.get(url);
     setIcon(res.data.weather[0].icon);
-  }, [props.cityName]);
+  };
 
   const iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
 
